c16e8b288c8e57d7c1282a0af35f4478665c00ba
Sumit Patel <sumit117.it@gmail.com>
2019-07-19 23:42:03 +0530

    setup

---
+++ api/controllers/FooController.js
@@ -0,0 +1,27 @@
+/**
+ * FooController
+ *
+ * @description :: Server-side actions for handling incoming requests.
+ * @help        :: See https://sailsjs.com/docs/concepts/actions
+ */
+var ObjectID = require('mongodb').ObjectID;
+module.exports = {
+
+  create: async (req, res) => {
+    let obj = {
+      firstName: 'sumit',
+      addresses: [{
+        id: new ObjectID(),
+        city: 'abc'
+      }, {
+        id: new ObjectID(),
+        city: 'efg'
+      }]
+    }
+
+    let foo = await Foo.create(obj).fetch();
+    res.status(201).json({
+      data: foo
+    });
+  }
+};
---
+++ api/models/Foo.js
@@ -0,0 +1,21 @@
+/**
+ * Foo.js
+ *
+ * @description :: A model definition represents a database table/collection.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+    firstName: {
+      type: 'string'
+    },
+    addresses: {
+      type: 'json',
+      columnName: 'array'
+    }
+
+  },
+
+};
